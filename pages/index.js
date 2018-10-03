b9b0e4e9b07e704d5ab75bbaa69d66fffe436f6d
Josiah Donor <jdonor@gmail.com>
2018-10-03 15:39:36 -0700

    Load basic budget data

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,25 @@
-export default () => <div>Testing, placeholder text here!</div>
+const ynab = require('ynab');
+const ynabApi = new ynab.API(process.env.ACCESS_TOKEN);
+
+const Index = props => (
+  <div>
+    <h1>Budget Tools</h1>
+
+    <ul>
+      {props.budgets.map(budget => (
+        <li key={budget.id}>{budget.name}</li>
+      ))}
+    </ul>
+  </div>
+);
+
+Index.getInitialProps = async function() {
+  const response = await ynabApi.budgets.getBudgets();
+  const budgets = await response.data.budgets;
+
+  return {
+    budgets: budgets,
+  };
+};
+
+export default Index;
